85acba7e5599486b7fb3007e308a26be7de09039
Dylan Nguyen <dmnguyen@cpp.edu>
2017-10-17 19:36:46 -0700

    Methods

--- src/LetterFreq.java
+++ src/LetterFreq.java
@@ -2,4 +2,62 @@
  * Created by dmnguyen on 10/17/17.
  */
 public class LetterFreq {
+    final String ALPHA = "abcdefghijklmnopqrstuvwxyz";
+    double[] frequency = { 0.1251, 0.0925, 0.0804, 0.0760, 0.0726, 0.0709, 0.0654, 0.0612, 0.0549, 0.0414, 0.0399,
+            0.0306, 0.0271, 0.0253, 0.0230, 0.0200, 0.0196, 0.0192, 0.0173, 0.0154, 0.0099, 0.0067, 0.0019, 0.0016,
+            0.0011, 0.0009};
+
+    public void firstLetter() {
+
+    }
+
+    public void secondLetter() {
+
+    }
+
+    public void thridLetter() {
+
+    }
+
+    public void lastLetter() {
+
+    }
+
+    public void followE() {
+
+    }
+
+    public void frequencyCheck() {
+
+    }
+
+    public void digraphCheck() {
+
+    }
+
+    public void trigraphCheck() {
+
+    }
+
+    public void doubleLetterCheck() {
+
+    }
+
+    public void twoLetterWordCheck() {
+
+    }
+
+    public void threeLetterWordCheck() {
+
+    }
+
+    public void fourLetterWordCheck() {
+
+    }
+
+    public void commonWordsCheck() {
+
+    }
+
+
 }
--- src/MonoAlpha.java
+++ src/MonoAlpha.java
@@ -2,4 +2,11 @@
  * Created by dmnguyen on 10/17/17.
  */
 public class MonoAlpha {
+    final String ALPHA = "abcdefghijklmnopqrstuvwxyz";
+    final String CIPHER_ALPHA = "abfgcedlmstuvnqrwhopikxyzj";
+
+    public MonoAlpha() {
+
+    }
+
 }
